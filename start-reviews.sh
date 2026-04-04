d19f504590a59a86721286a55a9ce699507a38f9
paalss <paalss94@gmail.com>
2026-04-04 17:03:06 +0200

    rename "portfolio-new" to "reviews"

--- start-reviews.sh
+++ start-reviews.sh
@@ -5,7 +5,7 @@ function sendKeys {
     tmux split-window -v -p 80
     tmux send-keys 'explorer.exe http://localhost:4321/' C-m
     # tmux send-keys 'cdtenant' C-m
-    tmux rename-window PORTFOLIO-NEW
+    tmux rename-window reviews
 }
 
 # If tmux session is not already open
